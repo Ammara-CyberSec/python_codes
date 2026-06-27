bdbd20c158375c727f1cacb6d953b9c3dede9266
Ammara-CyberSec <ammaraf5277941@gmail.com>
2026-06-27 20:14:27 +0500

    Day 1: String_practice_question

---
+++ string_practice2.py
@@ -0,0 +1,23 @@
+#PROGRAM 7:
+#Print last 3 characters of the string
+word = "Developer"
+print(f"Last 3 character: {word[-3:]}")
+
+
+#PROGRAM 8:
+#Find substring
+text = "I am learning python every day"
+print(" python" in text)
+
+#PROGRAM 9:
+#Count letter
+fruit = "Banana"
+print(f"Count a: {fruit.count('a')}")
+
+
+#PROGRAM 10:
+#Check end 
+word = "notes.txt"
+print(f"Is word end with .txt? {word.endswith('.txt')}")
+
+
