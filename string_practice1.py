de329113e541f2d0ec277e11d6234f5a14526469
Ammara-CyberSec <ammaraf5277941@gmail.com>
2026-06-27 20:13:15 +0500

    Day 1: String_practice

---
+++ string_practice1.py
@@ -0,0 +1,33 @@
+#PROGRAM 1:
+#Convert String in uppercase
+text = "Artificial intelligence"
+print(f"Text in uppercase: {text.upper()}")
+
+#PROGRAM 2:
+#Convert String in lowercase
+text = "PYTHON"
+print(f"Text in lowercase: {text.lower()}")
+
+
+#PROGRAM 3:
+#Capitalize only first letter
+text = "welcome to python"
+print(text.capitalize())
+
+
+#PROGRAM 4:
+#Replacing
+text = "Python Programming"
+print(f"Replace Programming with codding: {text.replace('Programming','Codding')}")
+
+
+#PROGRAM 5:
+#Removing extra space
+word = "   OpenAI  "
+print(f"Remove extra space in string: {word.strip()}")
+
+#PROGRAM 6:
+#Length of string
+subject = "Computer Science"
+print(f"Length is: {len(subject)}")
+
