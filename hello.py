3df2bc959ec56246c095c12358a2edc407b63921
AmmaraCyberSec <ammaraf5277941@gmail.com>
2026-06-27 11:25:55 +0500

    Day 1: Uploaded first Python programs
    First Python practice files uploaded to GitHub.

---
+++ hello.py
@@ -0,0 +1,8 @@
+# Program 1
+print("Hello World")
+
+# Program 2
+print("Hello Guyess")
+
+
+
---
+++ simple_input_and_calculation1.py
@@ -0,0 +1,25 @@
+#PROGRAM 1:
+#Simple input and Calculation
+n1 = int(input("Enter a number:"))
+square_n = n1*n1
+print("Square of ",n1,"=", square_n)
+
+
+#PROGRAM 2:
+# Simple input and Calculation
+n = int(input("Enter a number:"))
+cube_n = n*n*n
+print("Cube of",n, "=",cube_n)
+
+#PROGRAM 3:
+#Calculate  area of triangle
+base = float(input("Enter base :"))
+height = float(input("Enter height:"))
+Area = 1/2*(base*height)
+print("Area of trangle is:", Area)
+
+#PROGRAM 4:
+#Calculate Area of circle
+radius = float(input("Enter radius :"))
+area = 3.14*radius *radius
+print("Area of circle is:",area)
