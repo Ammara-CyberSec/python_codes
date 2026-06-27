773d17cca90e044f06634cefae92e20731200963
Ammara-CyberSec <ammaraf5277941@gmail.com>
2026-06-27 20:16:39 +0500

    Day 1: String_challenges_questions

---
+++ string_challenge_Q.py
@@ -0,0 +1,24 @@
+# PROGRAM 1:
+#Extract string
+text = "Cyber Security Expert"
+print(f"Extract Security from string: {text[6:14]}")
+
+#PROGRAM 2:
+#Reverse the string
+word = "Python"
+print(word[ : : -1])
+
+#PROGRAM 3:
+#Check equality
+word = "Apple"
+print("Apple" == "apple")
+
+#PROGRAM 4:
+#Find index
+text = "Hello World"
+print(f"Index of first occourrence of o: {text.index('o')}")
+
+#PROGRAM 5:
+#Print word many times without using loop
+text = "Python"
+print((text + "\n")*3)
