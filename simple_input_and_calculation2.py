d46febe26f7b6e0e81ea07e50f4e0527bb10da12
AmmaraCyberSec <ammaraf5277941@gmail.com>
2026-06-27 11:31:28 +0500

    Day 1: Uploaded first Python programs
    First Python practice files uploaded to GitHub.

---
+++ simple_input_and_calculation2.py
@@ -0,0 +1,30 @@
+#PROGRAM 5:
+# Convert temperature Celsius into Fahrenheit
+c = float(input("Enter temperature in celsius:"))
+fahrenheit = (c*9/5) + 32
+print("Temperature in Fahrenheit is:",fahrenheit)
+
+#PROGRAM 6:
+#Calculate parameter of a side of square
+length = int(input("Enter length:"))
+perimeter = 4*length
+print("Perimeter of square is:",perimeter)
+
+
+#PROGRAM 7:
+#Calculate average of 5 numbers
+n1 = int(input("Enter 1 number:"))
+n2 = int(input("Enter 2 number:"))
+n3 = int(input("Enter 3 number:"))
+n4 = int(input("Enter 4 number:"))
+n5 = int(input("Enter 5 number:"))
+Average = (n1+n2+n3+n4+n5)/5
+print("Average of five numbers:",Average)
+
+
+#PROGRAM 8:
+#Calculate Half of number
+n = int(input("Enter a number:"))
+half_n = n/2
+print("Half of",n,"is :",half_n)
+
