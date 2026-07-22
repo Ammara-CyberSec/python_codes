068b1f88cd687c38b1a2b8f54abcb1015b46a4c2
Ammara-CyberSec <ammaraf5277941@gmail.com>
2026-07-22 19:10:05 +0500

    Pseudo code

---
+++ Pseudo_code_1.py
@@ -0,0 +1,55 @@
+# Pseudo Code 9:
+# Sum of n number
+START
+SET Sum = 0
+INPUT n
+FOR i =1 to n Do
+	SET Sum = Sum +i
+END FOR
+DISPLAY Sum
+END
+
+# Pseudo Code 10:
+# Table of 5
+START
+SET n = 5
+FOR i = 1 to 10 Do
+	Result = n*i
+	DISPLAY Result
+END FOR
+END
+
+
+# Pseudo Code 11:
+# Take number to user until 0 will be entered
+START
+SET count = 0
+WHILE True Do
+	INPUT n
+	IF n > 0 THEN
+		SET count = count +1
+	ELSE
+		BREAK
+END WHILE
+DISPLAY count
+END
+
+
+# Pseudo Code 12:
+# Sum of only Even nber
+START
+SET i = 1
+SET Sum = 0
+WHILE i <= 100 Do
+		lF i MOD 2 = 0 THEN
+			SET Sum = Sum + i
+		END IF
+		SET i = i +1
+END WHILE
+DISPLAY Sum
+END
+
+
+			
+		
+	
