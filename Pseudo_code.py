a9334d50c9c0a528916eef75acd2ba4a7cbe5fad
Ammara-CyberSec <ammaraf5277941@gmail.com>
2026-07-22 18:44:39 +0500

    Pseudo code document

---
+++ Pseudo_code.py
@@ -0,0 +1,97 @@
+# Pseudo Code 1;
+# Check Number is Positive,Negative or Zero
+START
+INPUT number
+IF number > 0 THEN
+	DISPLAY Positive number
+ELSE IF number < 0 THEN
+	DISPLAY  Negative number
+ELSE 
+	DISPLAY Zero
+	END IF
+END
+
+
+# Pseudo Code 2:
+# Print Greater number
+START
+INPUT number1
+INPUT number2
+IF number1 > number2 THEN
+	DISPLAY number1 is greater
+ELSE
+	DISPLAY number2 is greater
+	END IF
+END
+
+
+# Pseudo Code 3:
+# Print Smallest number
+START
+INPUT number1
+INPUT number2
+INPUT number3
+IF number1 < number2 AND number1 < number3  THEN
+	DISPLAY number1 is smallest
+ELSE IF number2 < number1 AND  number2 < number3  THEN
+	DISPLAY number2 is smallest
+ELSE
+	DISPLAY number3 is smallest
+	END IF
+END
+
+# Pseudo Code 4:
+# Check Eligibility Criteria
+START
+INPUT age
+IF age >= 18 THEN
+	DISPLAY Eligible to vote
+ELSE
+	DISPLAY Not Eligible to vote
+	END IF
+END
+
+
+#  Pseudo Code 5:
+# Area of Rectangle
+START
+INPUT length
+INPUT Width
+SET Rect_area = length*Width
+DISPLAY Rect_area
+END
+
+
+# Pseudo Code 6:
+# Simple interest
+START
+INPUT P
+INPUT R
+INPUT T
+SI =(P*R*T)/100
+Total_p = P + SI
+DISPLAY SI
+DISPLAY  Total_p
+END
+
+# Pseudo Code 7:
+# Print 1 to 20 counting
+START 
+SET n 
+FOR i= 1 to 20 Do
+	DISPLAY i
+END FOR
+END
+
+# Pseudo Code 8:
+# Print 20 to 1 
+START
+SET n = 20
+WHILE n >= 1 Do
+	DISPLAY  n
+	SET n = n-1
+END WHILE
+END
+	
+
+
